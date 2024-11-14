d04759891e565e97d7da7c02abbc1cc445ad0585
tiberchsl <tibercarlitohsl@gmail.com>
2024-11-14 19:11:57 -0300

    Añadido comentario para hacer logout de github en cmd, archivo FirstActivity

--- app/src/main/java/com/example/aprendiendo/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/aprendiendo/firstapp/FirstAppActivity.kt
@@ -6,7 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aprendiendo.R
-
+// https://stackoverflow.com/questions/28238037/git-log-out-user-from-command-line para hacer logout
+// de github a traves de cmd
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
