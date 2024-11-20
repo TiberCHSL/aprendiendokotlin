129e9a20bc831e3158f29e2361f2db75b1f50573
tiberchsl <csaldes03@gmail.com>
2024-11-19 21:07:57 -0300

    Aplicacion qla que te saluda, el layout esta entero feo

--- app/src/main/java/com/example/aprendiendo/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/aprendiendo/firstapp/FirstAppActivity.kt
@@ -1,18 +1,37 @@
 package com.example.aprendiendo.firstapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aprendiendo.R
 // https://stackoverflow.com/questions/28238037/git-log-out-user-from-command-line para hacer logout
 // de github a traves de cmd
+
+
+
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app) //diseño de la actividad
+        val botonqlo = findViewById<AppCompatButton>(R.id.botonqlo)     // codigo para pasarle a la actividad la vista botonqlo
+        val editando = findViewById<AppCompatEditText>(R.id.editando)
+        botonqlo.setOnClickListener{
+            val name = editando.text.toString()
+            if (name.isNotEmpty()){
+                Log.i("Donwea","se pulso el botonqlo $name} ")
+                val intent = Intent(this, SaludoActivity::class.java)
+                intent.putExtra("EXTRA_NAME",name)
+                startActivity(intent)
+            }
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,3 +39,5 @@ class FirstAppActivity : AppCompatActivity() {
         }
     }
 }
+
+//campos de texto es sp, para layout es dp
---
+++ app/src/main/java/com/example/aprendiendo/firstapp/SaludoActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aprendiendo.firstapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aprendiendo.R
+
+class SaludoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_saludo)
+
+        val saludoql = findViewById<TextView>(R.id.saludoql) // pasarle la vista a la actividad
+        val name: String =intent.extras?.getString("EXTRA_NAME").orEmpty()
+        saludoql.text = "Wena po $name"
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
